eb2702fff6a20fcd513c18482c6639bbb814b99b
Pragya Singh <64683624+pragya7156@users.noreply.github.com>
2021-06-14 14:40:57 +0530

    first commit

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', require('./api/index'));
 app.use('/users', require('./routes/users'));
 
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static("client/build"))
+    app.get('/*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 app.listen(PORT, () => {
     console.log(`Server started at Port ${PORT}`);
 });
